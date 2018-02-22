f6af49f5e11febf032851dcd5052073a480fd92f
Billy Douglas <jwdoug01@gmail.com>
2018-02-22 15:00:51 -0500

    adding support for pulse. so far, grabbed data, but have not tried to pulse yet

--- TriggerDino.ino
+++ TriggerDino.ino
@@ -8,11 +8,11 @@ int read_line(char* buffer, int bufsize)
 		}
 
 		char ch = Serial.read(); // read next character
-// jwd		Serial.print(ch); // echo it back: useful with the serial monitor (optional)
+		Serial.print(ch); // echo it back: useful with the serial monitor (optional)
 		// jwd may want to check for other chars since \n didn't work
 		if ((ch == '\r')||(ch == '\n') ) {
 			buffer[index] = 0; // end of line reached: null terminate string
-			// jwdSerial.println(ch); // echo it back: useful with the serial monitor (optional)
+			Serial.println(ch); // echo it back: useful with the serial monitor (optional)
 			return index; // success: return length of string (zero if string is empty)
 		}
 
@@ -24,12 +24,12 @@ int read_line(char* buffer, int bufsize)
 
 	char ch;
 	do {
-	//jwd	Serial.print("clearning buffer ");
+		Serial.print("clearning buffer ");
 		// Wait until characters are available
 		while (Serial.available() == 0) {
 		}
 		ch = Serial.read(); // read next character (and discard it)
-		// jwdSerial.print(ch); // echo it back
+		Serial.print(ch); // echo it back
 	} while (ch != '\n');
 
 	buffer[0] = 0; // set buffer to empty string even though it should not be used
@@ -60,11 +60,11 @@ int getPin(char *str, struct PinType *pin) {
 		strncpy(pinString,&str[1],sizeof(pinString)-1);
 		pin->number=atoi(pinString);
 		if ((pin->type == 'D') && ((pin->number < 0) || (pin->number > MAX_DIGITAL))){
-			//jwdSerial.println("Digital pin out of range");
+			Serial.println("Digital pin out of range");
 			rc=-1;
 		}
 		else if ((pin->type == 'A') && ((pin->number < 0) || (pin->number > MAX_ANALOG))){
-			//jwdSerial.println("Analog pin out of range");
+			Serial.println("Analog pin out of range");
 			rc=-1;
 		}
 	}
@@ -74,33 +74,61 @@ int getPin(char *str, struct PinType *pin) {
 int  validateLevel(char *str){
 	int rc = 0;
 	if (str == NULL) {
-	//jwd	Serial.println("Error: no level supplied");
+			Serial.println("Error: no level supplied");
 		rc = -1;
 	}
 	else {
 		rc = atoi(str);
-		// jwdSerial.println(rc);
+		Serial.println(rc);
 	}
 	if ((rc != 0) && (rc !=1)) {	
-		//jwdSerial.println("Error: invalid level");
+		Serial.println("Error: invalid level");
 		rc = -1;
 	}
 	return rc;
 }
-void loop() {
-	// jwdSerial.print("> ");
+long getDuration(char *str) {
+	long rc = 1;
+	int strLen=0;
+	long multiplier=1000000; /* if units is seconds, convrt to us */
 
-	// Read command
+	if (str == NULL) {
+		Serial.println("Error: no duration supplied");
+		rc = 0;
+	}
+	else if ((strLen=strlen(str))>=2) {
+		/* look for multiplier */
+		char lastChar = str[strLen-1];
+		if (lastChar == 'm'){
+			multiplier=1000;
+			str[strLen-1]='\0';
+		}
+		else if (lastChar == 'u'){
+			multiplier=1;
+			str[strLen-1]='\0';
+		}
+	}
 
+	if (rc) rc = atoi(str)*multiplier;
+	
+	return rc;
+	
+}
+
+void parseCommand() {
 	char command[LINE_BUFFER_SIZE];
+	char * pch;
+	struct PinType pin{'U',-1};
+
 	if (read_line(command, sizeof(command)) < 0) {
-		//jwdSerial.println("Error: line too long");
+		Serial.println("Error: line too long");
 		return; // skip command processing and try again on next iteration of loop
 	}
 
+	for (int i=0; i< strlen(command);i++){
+		command[i]=toupper(command[i]);
+	}
 
-	char * pch;
-	struct PinType pin{'U',-1};
 
 	pch = strtok (command," ");
 	while (pch != NULL)
@@ -116,50 +144,90 @@ void loop() {
 					pinMode(pin.number,OUTPUT);
 					digitalWrite(pin.number,(level==0)?LOW:HIGH);
 				}
-				// configure if need be, or just set. add param for configured to struct?
-			
 			}
-		} else if (strcmp(command, "IN") == 0) {
-		} else {
-			//jwdSerial.print("Error: unknown command: \"");
-			//jwdSerial.print(command);
 		}
-			
+		else if (strcmp(pch, "PULSE") == 0) {
+			int level, repeat;
+			long pulseDuration;
+			long postDuration=0;
+			pch = strtok (NULL, " ");
+			if ((level = validateLevel(pch)) < 0){
+				
+			}
+			else if (( pulseDuration = getDuration(pch = strtok (NULL, " "))) < 0){
+				Serial.print("error pulseDuration: ");
+			}
+			else if (( postDuration = getDuration(pch = strtok (NULL, " "))) < 0){
+				Serial.print("error postDuration: ");
+			}
+			else {
+				 repeat = atoi(pch = strtok (NULL, " "));
+			}
+			Serial.print("pulseDuration: ");Serial.println(pulseDuration);
+			Serial.print("postDuration: ");Serial.println(postDuration);
+			Serial.print("repeat: ");Serial.println(repeat);
+
+
+//				if (pin.type == 'D') {
+//					pinMode(pin.number,OUTPUT);
+//					digitalWrite(pin.number,(level==0)?LOW:HIGH);
+//				}
+		}
+		else if (strcmp(command, "IN") == 0) {
+		}
+		else {
+			Serial.print("Error: unknown command: \"");
+			Serial.print(command);
+		}
+
 		pch = strtok (NULL, " ");
 	}
+}
+
+
+
+void loop() {
+	
+	/* handle timers */
+
+	if (Serial.available() != 0) {
+		parseCommand();
+	}
+
+
 
 }
 #if 0
 
-	> D10 OUT 0
-	> D10 PULSE HIGH 10u /* set low, keep high for 10us, drop low */
-	> D10 PULSE HIGH 10m /* set low, keep high for 10ms, drop low */
-	> D10 PULSE HIGH 10 /* same, but for 10s*/ 
-	> D10 pulse High 10u 1u /* same as high pulse, but delay 1u after low */ 
-	> D10 pulse High 10u 1u 5 /* same as high pulse, but delay 1u after low, repeat 5 times */ 
-	> D10 PWM 40 /* Duty cycle = 40%, stays low 40%, high 60%*/
+> D10 OUT 0
+> D10 PULSE HIGH 10u /* set low, keep high for 10us, drop low */
+> D10 PULSE HIGH 10m /* set low, keep high for 10ms, drop low */
+> D10 PULSE HIGH 10 /* same, but for 10s*/ 
+> D10 pulse High 10u 1u /* same as high pulse, but delay 1u after low */ 
+> D10 pulse High 10u 1u 5 /* same as high pulse, but delay 1u after low, repeat 5 times */ 
+> D10 PWM 40 /* Duty cycle = 40%, stays low 40%, high 60%*/
 
 
-	// 1) PIN - can have format D1, D12, A0
-	// 2) direction
-	// 3) if output, value
-	// 4) later, if input return value
-	// 5) other options: PWM, pulse (need direction, duration; repeat
+// 1) PIN - can have format D1, D12, A0
+// 2) direction
+// 3) if output, value
+// 4) later, if input return value
+// 5) other options: PWM, pulse (need direction, duration; repeat
 
-	/*
+/*
 
-	   PIN #:  can have format D0 - D19, A0-A7
-		FUNCTION: OUT, IN
-		OUT: LEVEL, PULSE, PWM
-		0/1: Set Level
-	PULSE:
-		Direction (L/H), Duration
-		Repeat, time b/w pulses
-	PWM: set duty cycle
+   PIN #:  can have format D0 - D19, A0-A7
+   FUNCTION: OUT, IN
+   OUT: LEVEL, PULSE, PWM
+   0/1: Set Level
+   PULSE:
+   Direction (L/H), Duration
+   Repeat, time b/w pulses
+   PWM: set duty cycle
 
-IN: for now, read ADC value or digital value
-	Later: read pwm?
+   IN: for now, read ADC value or digital value
+   Later: read pwm?
 
-	 */
+ */
 
 #endif
